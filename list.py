96caac3b207f745b3958fea1970d8ec578afce7f
muratkutlutuna <muratkutlutuna@gmail.com>
2022-06-30 16:23:05 +0200

    study

---
+++ list.py
@@ -0,0 +1,19 @@
+# my_list=[1,2,3]
+# my_list=['bir',2,True,5.6]
+# print(my_list)
+list1=['one','two','three']
+list2=['four','five','six']
+numbers = list1+list2
+print(numbers)
+print(len(numbers))
+print(numbers[2])
+userA=['Sadik',36]
+userB=['Cinar',2]
+users=userA+userB
+users2=userA,userB
+print(users2)
+print(users2[0][0][0])
+print(userA)
+print(userB)
+print(users)
+print(users[0][0])
--- string-methods-demo.py
+++ string-methods-demo.py
@@ -3,9 +3,15 @@ course = "Python Kursu: Bastan Sona Python Progamlama Rehberiniz (40 saat)"
 
 # 1- ' Hello World ' karakter dizisinin bas ve sondaki bosluk karakterlerini silin.
 hw = " Hello World "
+result = ' Hello World '.strip()
+result = ' Hello World '.lstrip()
+result = ' Hello World '.rstrip('rld ')
+
+print(result)
 print(hw.strip())
 # 2- 'www.sadikturan.com' icindeki sadikturan yazisi haricindeki her karakteri silin.
 print(website[11:21])
+print(website.strip('htp/:w.com'))
 # 3- 'course' karakter duizisinin tum karakterlerini kucuk harf yapin
 print(course.lower())
 # 4- 'website' icinde kac tane a karakteri vardir ? (count('a'))
@@ -17,14 +23,18 @@ bitiyormu = website.endswith("com")
 print(f"website .com ile bitiyor mu? = {bitiyormu}")
 # 6- 'website' icinde '.com' ifadesi var mi?
 comVarMi = website.__contains__(".com")
+print(website.find('.com'))
 print(comVarMi)
 # 7- 'course' icindeki karakterlerin hepsi alfabetik mi?
 print(course.isalpha())
 # 8- 'contents' ifadesini satirda 50 karakter icine yerlestirip sag ve soluna * ekleyiniz.
 str = 'contents'
+print(str.ljust(50,'*'))
+print(str.rjust(50,'*'))
 print(str.center(50, '*'))
 # 9- 'course' karakter dizisindeki tum bosluk karakterlerini '-' ile degistirin.
 print(course.replace(" ", "-"))
+print(course.replace(' ','-',5))
 # 10- 'Hello World' karakter dizisinin 'World ifadesini 'There' olarak degistirin
 print(hw.strip().replace("World", "There"))
 # 11- 'course' karakter dizisini bosluk karakterlerinden ayirin.
