63add1a822cc5b22e5873dc8b2530fc453dd8010
muratkutlutuna <muratkutlutuna@gmail.com>
2022-09-09 06:45:19 +0200

    study

---
+++ modules/math.py
@@ -0,0 +1,28 @@
+# Yontem 1
+# import  math
+# import math as islem
+
+# value = dir(math)
+# value = help(math)
+# value = help(math.factorial)
+# value = math.sqrt(49)
+# value = math.factorial(5)
+# value = math.floor(5.9)
+# value = math.ceil(5.9)
+
+# value = islem.factorial(5)
+
+# Yontem 2
+# def sqrt(x):
+#     print('x : '+str(x))
+
+from math import factorial,sqrt,ceil
+
+def sqrt(x):
+    print('x : '+str(x))
+
+# vlue = factorial(5)
+value = sqrt(9)
+# value = ceil(9.8)
+
+print(value)
---
+++ modules/mod.py
@@ -0,0 +1,26 @@
+'''
+    Information about module
+'''
+print('modul eklendi')
+
+number = 10
+
+numbers = [1,2,3]
+
+person ={
+    "name": "Ali",
+    "age": "25",
+    "city": "istanbul"
+}
+
+def func(x):
+    '''
+        information about function
+    :param x:
+    :return:
+    '''
+    print(f'x:{x}')
+
+class Person:
+    def speak(self):
+        print('I am speaking...')
